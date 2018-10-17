a31071273f756dea499efe22a1887dccca36137f
katrina hockman and Phil Mass <phil@>
2018-10-17 10:57:33 -0700

    add multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Back-end business logic
 var add = function(number1, number2){
   return number1 + number2;
 };
@@ -15,6 +16,7 @@ var divide = function(number1, number2) {
 };
 
 
+// Front-end user interface logic
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
@@ -23,4 +25,12 @@ $(document).ready(function() {
     var result = add(number1, number2);
     $("#output").text(result);
   });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  })
 });
